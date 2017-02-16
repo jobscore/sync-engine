505fad4e2d10c3d6df560a5bbba44250b6ac12f1
André Diego Piske <andre@jobscore.com>
2017-02-16 15:02:00 -0200

    Fix setup.sh
    Work around issue https://github.com/nylas/sync-engine/issues/431

--- setup.sh
+++ setup.sh
@@ -178,6 +178,10 @@ pip install 'pip==8.1.2' 'setuptools>=5.3'
 hash pip        # /usr/bin/pip might now be /usr/local/bin/pip
 pip install 'pip==8.1.2' 'setuptools>=5.3'
 
+sed -i '59s/MARKER_EXPR()/MARKER_EXPR("")/g' /usr/local/lib/python2.7/dist-packages/packaging/requirements.py
+
+easy_install -U pyasn1 setuptools
+
 # Doing pip upgrade setuptools leaves behind this problematic symlink
 rm -rf /usr/lib/python2.7/dist-packages/setuptools.egg-info
 
